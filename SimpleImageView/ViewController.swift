dcde8e3200f6ff87e7cb23da1918a70294221236
Lawrence Lee <bronzetunatwo@yahoo.com>
2015-12-08 08:51:22 -0500

    Initial Commit

---
+++ SimpleImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleImageView
+//
+//  Created by Deep Thought on 12/8/2015.
+//  Copyright © 2015 Lawrence Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
